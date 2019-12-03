e0c0beb0a6400ea65e5ae4a5dbe9019010832a1f
T Benson <t.benson@live.co.uk>
2019-12-03 16:22:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,8 +7,8 @@ def load_library(file_name)
   emoticons_hash = {:get_meaning => {}, :get_emoticon => {}}
   emoticons_file.each do |key, values|
 
-      emoticons_hash[:get_meaning] = {"#{values[1]}" => "#{key}"}
-      emoticons_hash[:get_emoticon] = {"#{values[0]}" => "#{values[1]}"}
+      emoticons_hash[:get_meaning][values[1]] = key
+      emoticons_hash[:get_emoticon][values[0]] = values[1]
   end
       binding.pry
   emoticons_hash
