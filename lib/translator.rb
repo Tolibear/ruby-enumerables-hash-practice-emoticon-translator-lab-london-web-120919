9d90748870e24bf5c6fca56b0640ab94777f2546
T Benson <t.benson@live.co.uk>
2019-12-03 16:23:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ def load_library(file_name)
       emoticons_hash[:get_meaning][values[1]] = key
       emoticons_hash[:get_emoticon][values[0]] = values[1]
   end
-      binding.pry
+      # binding.pry
   emoticons_hash
 end
 
