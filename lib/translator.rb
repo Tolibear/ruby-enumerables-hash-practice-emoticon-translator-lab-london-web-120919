898f3d19b08775f4f1b26b604736ed4a093b59e0
T Benson <t.benson@live.co.uk>
2019-12-03 15:25:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,21 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file_name)
+  emoticons_array = :YAML.load_file(file_name)
+
+  emoticons_hash = {:get_meaning => {}, :get_emoticon => {}}
+  emoticons_array.key.each do |english, japanese|
+    # values.each do |english, japanese|
+      # japanese.each do |img_japanese|
+    #     japanese_emc = img_japanese.to_sym
+    #     emoticons_hash[:get_meaning] = { japanese_emc => key }
+    #
+    # # emoticons_hash[:get_meaning] = {}
+    emoticons_hash[:get_meaning][japanese.to_s] = key
+    # emoticons_hash[:get_emoticon] = {}
+      # end
+    # end
+  end
 end
 
 def get_japanese_emoticon
