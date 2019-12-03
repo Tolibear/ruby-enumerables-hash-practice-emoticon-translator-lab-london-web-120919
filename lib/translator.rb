a5b635f1dbd3f5c0017245117c29f2e756b4c5b3
T Benson <t.benson@live.co.uk>
2019-12-03 15:48:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,11 +6,9 @@ def load_library(file_name)
   emoticons_hash = {:get_meaning => {}, :get_emoticon => {}}
   emoticons_file.each do |key, values|
     values.each do |english, japanese|
-
+      emoticons_hash[:get_meaning] = {japanese : english}
   #       japanese_emc = img_japanese.to_sym
   #       emoticons_hash[:get_meaning] = { japanese_emc => key }
-
-      print key
 
     end
   end
